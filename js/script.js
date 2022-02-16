e8405b18921009a218a126a0f228cc18113e993d
leighannfrench <leighannfrench20@gmail.com>
2022-02-16 14:21:45 -0500

    more js changes for three images.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-let imgPlay = ["img/placeholder.jpg", "img/placeholder2.jpg"];
+let imgPlay = ["img/placeholder.jpg", "img/placeholder2.jpg","img/placeholder3.jpg","img/placeholder4.jpg", "img/placeholder5.jpg", "img/placeholder6.jpg", "img/placeholder7.jpg", "img/placeholder8.jpg"];
 let whichImg = 0;
 let image = document.querySelector("#marquee");
-
+let image1 = document.querySelector("#marquee1");
+let image2 = document.querySelector("#marquee2");
 
 const nextImg = () =>{
   whichImg =( whichImg + 1 > imgPlay.length - 1) ? 0 : whichImg + 1;
@@ -9,6 +10,18 @@ const nextImg = () =>{
   
 }
 document.querySelector("#next").addEventListener("click", nextImg);
+const nextImg1 = () =>{
+  whichImg =( whichImg + 1 > imgPlay.length - 1) ? 0 : whichImg + 1;
+  image1.src = imgPlay[whichImg];
+ 
+}
+document.querySelector("#next").addEventListener("click", nextImg1);
+const nextImg2 = () =>{
+  whichImg =( whichImg + 1 > imgPlay.length - 1) ? 0 : whichImg + 1;
+  image2.src = imgPlay[whichImg];
+
+}
+document.querySelector("#next").addEventListener("click", nextImg2);
 
 const preImg = () =>{
   whichImg--;
@@ -16,3 +29,15 @@ const preImg = () =>{
   image.src = imgPlay[whichImg];
 }
 document.querySelector("#previous").addEventListener("click", preImg);
+const preImg1 = () =>{
+  whichImg--;
+  if(whichImg < 0) whichImg = imgPlay.length - 1;
+  image1.src = imgPlay[whichImg];
+}
+document.querySelector("#previous").addEventListener("click", preImg1);
+const preImg2 = () =>{
+  whichImg--;
+  if(whichImg < 0) whichImg = imgPlay.length - 1;
+  image2.src = imgPlay[whichImg];
+}
+document.querySelector("#previous").addEventListener("click", preImg2);
